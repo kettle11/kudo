c671d02f59f602d4a9f91f4fd026eb3b5a92fe0b
Ian Kettlewell <ian.kettlewell@gmail.com>
2020-09-10 15:02:37 -0700

    Update example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,19 +1,12 @@
 use kudo::*;
 
-struct A(f32);
-struct B(f32);
 fn main() {
     let mut world = World::new();
     world.spawn((true,));
-    world.spawn((true, 10.));
+    world.spawn((false, 10.));
 
-    let queryable = world.into_queryable_world();
-    {
-        let mut query = queryable.query::<(&bool,)>();
-        {
-            let iter = query.iterator();
-            //  for q in iter {}
-        }
+    let mut q = world.query::<(&bool,)>();
+    for i in q.iterator() {
+        println!("i: {:?}", i);
     }
-    println!("HELLO");
 }
