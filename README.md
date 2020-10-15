9841864de7c211761f98f573036fe1d05727960c
Ian Kettlewell <ian.kettlewell@gmail.com>
2020-10-14 23:11:09 -0700

    Update examples and README

--- README.md
+++ README.md
@@ -7,8 +7,6 @@ A simple Entity Component System for Rust.
 * Zero `unsafe` (so far)
 
 ```rust 
-let mut world = World::new();
-
 struct Health(f32);
 struct Name(String);
 struct CreepySnakeHair(u32);
@@ -17,13 +15,17 @@ let mut world = World::new();
 
 // Create entities with components.
 world.spawn((Name("Perseus".to_string()), Health(50.)));
-world.spawn((Name("Medusa".to_string()), Health(100.), CreepySnakeHair(300)));
+world.spawn((
+    Name("Medusa".to_string()),
+    Health(100.),
+    CreepySnakeHair(300),
+));
 
 // Find every entity with a `Name` and a `Health` component.
-let mut query = world.query::<(&Name, &Health)>().unwrap();
+let mut query = world.query::<(&Name, &Health)>();
 
 // Iterate through all entities with those components.
 for (name, health) in query.iter() {
-    println!("{:?}'s health is: {:?}", name, health);
+    println!("{}'s health is: {:?}", name.0, health.0);
 }
 ```
--- examples/function_system.rs
+++ examples/function_system.rs
@@ -1,5 +1,5 @@
-//! This example is the same as `hello.rs` except a function is used as the
-//! system.
+//! This example is the same as `hello.rs` except a function is used as the system
+//! and multiple queries are passed into the system.
 
 use kudo::*;
 
@@ -19,10 +19,18 @@ fn main() {
     ));
 
     // Find every entity with a `Name` and a `Health` component.
-    fn print_names(mut query: Query<(&Name, &Health)>) {
+    fn print_names(
+        mut query: Query<(&Name, &Health)>,
+        mut creepy_hair: Query<(&Name, &CreepySnakeHair)>,
+    ) {
         // Iterate through all entities with those components.
         for (name, health) in query.iter() {
             println!("{}'s health is: {:?}", name.0, health.0);
+        }
+
+        // Iterate through all entities with those components.
+        for (name, _) in creepy_hair.iter() {
+            println!("{} has creepy snake hair", name.0);
         }
     }
 
