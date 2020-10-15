627a286304d69d51d7f13dfcc4569136eee794f0
Ian Kettlewell <ian.kettlewell@gmail.com>
2020-10-14 23:02:44 -0700

    Added example of a function system

---
+++ examples/function_system.rs
@@ -0,0 +1,30 @@
+//! This example is the same as `hello.rs` except a function is used as the
+//! system.
+
+use kudo::*;
+
+fn main() {
+    struct Health(f32);
+    struct Name(String);
+    struct CreepySnakeHair(u32);
+
+    let mut world = World::new();
+
+    // Create entities with components.
+    world.spawn((Name("Perseus".to_string()), Health(50.)));
+    world.spawn((
+        Name("Medusa".to_string()),
+        Health(100.),
+        CreepySnakeHair(300),
+    ));
+
+    // Find every entity with a `Name` and a `Health` component.
+    fn print_names(mut query: Query<(&Name, &Health)>) {
+        // Iterate through all entities with those components.
+        for (name, health) in query.iter() {
+            println!("{}'s health is: {:?}", name.0, health.0);
+        }
+    }
+
+    print_names.run(&world);
+}
