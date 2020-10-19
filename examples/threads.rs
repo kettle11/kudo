4cb5349cbd979e8e3ab9e3feb23e71e6e99e5665
Ian Kettlewell <ian.kettlewell@gmail.com>
2020-10-19 15:33:13 -0700

    Fix threads example

--- examples/threads.rs
+++ examples/threads.rs
@@ -14,7 +14,7 @@ fn main() {
 
     // Run a query on another thread.
     let thread = std::thread::spawn(move || {
-        let mut query = world_other_thread.query::<(&bool,)>().unwrap();
+        let mut query = world_other_thread.query::<&bool>().unwrap();
         for b in query.iter() {
             println!("Boolean: {:?}", b);
         }
@@ -22,13 +22,13 @@ fn main() {
 
     // Even though this query accesses the same data as the other thread
     // it's ok because both threads are only reading the data.
-    let mut query = world.query::<(&bool,)>().unwrap();
+    let mut query = world.query::<&bool>().unwrap();
     for b in query.iter() {
         println!("Boolean: {:?}", b);
     }
 
     // This is also OK because the query does not overlap with the query on the other thread
-    let mut query = world.query::<(&mut i32,)>().unwrap();
+    let mut query = world.query::<&mut i32>().unwrap();
     for i in query.iter() {
         println!("I: {:?}", i);
     }
